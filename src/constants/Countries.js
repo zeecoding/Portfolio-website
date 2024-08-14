d01108b23b2aaf32d04465b148ee3eee0c2c608d
zeecoding <zubairwah682@gmail.com>
2024-08-14 12:55:48 +0500

    added contact section

---
+++ src/constants/Countries.js
@@ -0,0 +1,25 @@
+export const fetchCountryNames = async () => {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const countries = await response.json();
+    const formattedCountries = countries.map((country) => {
+      const suffixes = country.idd.suffixes || [];
+      const suffix = suffixes.length <= 1 ? suffixes[0] : "";
+
+      return {
+        name: country.name.common, // Country name
+        code: country.cca3, // ISO Alpha-3 country code
+        callingCode: country.idd.root + suffix,
+        flag: country.flags.svg
+      };
+    });
+    formattedCountries.sort((a, b) => a.name.localeCompare(b.name));
+    return formattedCountries;
+    } catch (error) {
+    console.error("Error fetching country names:", error);
+    throw error;
+  }
+};
